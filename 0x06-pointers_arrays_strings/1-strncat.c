09a53b3baa63e0781f2a616f0b32a06854abc6c5
manu-tech-code <ekwabena57@gmail.com>
2022-10-28 23:28:04 +0000

    update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,6 +8,25 @@
  * Return: concatenated string
  */
 
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0') /*i is index of null terminator*/
+		i++;
+
+	while (src[j] != src[n]) /*append replacing null terminator*/
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
+/* another method
 
 char *_strncat(char *dest, char *src, int n)
 {
@@ -15,7 +34,6 @@ char *_strncat(char *dest, char *src, int n)
    while (*dest)
        dest++;
 
-   int i = 0;
    // replace dest while src exists and insert nulls till n bytes
    for (i = 0; i < n; i++)
    {
@@ -28,4 +46,4 @@ char *_strncat(char *dest, char *src, int n)
 	*dest = '\0';
    }
 }
-
+*/
