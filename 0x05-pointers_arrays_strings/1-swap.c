9e56dd7109307069ada2afefb03981c63c72fa86
Emmanuel Larbi <117349144+manuel-larbi@users.noreply.github.com>
2022-11-23 08:52:56 +0000

    update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,9 +6,9 @@
  */
 void swap_int(int *a, int *b)
 {
-  int swap;
+int swap;
 
-  swap = *a;
-  *a = *b;
-  *b = swap;
+swap = *a;
+*a = *b;
+*b = swap;
 }
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,23 +1,21 @@
-  #include "main.h"
+#include "main.h"
 
+/**
+ * print_rev- print reversed string, followed by a new line
+ * @s: pointer to the string to point
+ * Return: void
+ */
 
-  /** 
-   * print_rev- print reversed string, followed by a new line
-   * @s: pointer to the string to point 
-   * Return: void
-   */
+void _print_rev(char *s)
+{
+int swap;
 
-   void _print_rev(char *s) 
-  {
-     int swap;
-
-     int i = 0;
-     while (s[i]) 
-            i++;
-  while (i--) 
-{  
-      _putchar(s[i]);
-  } 
-     _putchar('/n');
-
-  } 
+int i = 0;
+while (s[i])
+   i++;
+while (i--)
+{
+   _putchar(s[i]);
+}
+_putchar('/n');
+}
