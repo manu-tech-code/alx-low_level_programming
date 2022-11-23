0d74da5af4b01a4646cf740d3f2f64f5b4d4d6f3
Emmanuel Larbi <117349144+manuel-larbi@users.noreply.github.com>
2022-11-23 08:41:07 +0000

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,12 @@
 #include "main.h"
-  /**
-  * reset_to_98 - reset argument to 98
-  * @n: printed integer
-  * Return: void
-  */
+/**
+ * reset_to_98 - reset argument to 98
+ * @n: printed integer
+ * Return: void
+ */
 
-  void reset_to_98(int *n) 
-  { 
+void reset_to_98(int *n)
+{
 
     *n = 98;
-
- } 
-  
+}
--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,26 @@
-  #include "main.h"
+#include "main.h"
 
-  /** 
-  * prints_half - printing half a string 
-  * @str: that's the string
-  * 
-  * Return: void 
-  */
-    void puts_half(char *str)
- {
-    int length = 0;
-     char *start = str;
-
-    while (*str)
-      length++;
-       str++;
-}
-     length  += 1;
-      start += length / 2;
-         while(*start) 
+/**
+ * prints_half - printing half a string
+ * @str: that's the string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
 {
-      _putchar(*(start));
-         start++;
-} 
-     _putchar('\n');
-   } 
+  int length = 0;
+  char *start = str;
 
+  while (*str)
+    length++;
+  str++;
+}
+length += 1;
+start += length / 2;
+while (*start)
+{
+  _putchar(*(start));
+  start++;
+}
+_putchar('\n');
+}
