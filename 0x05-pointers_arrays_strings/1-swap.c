b68c480361e5435100cf3b94dfa48247b0cf6305
Emmanuel Larbi <117349144+manuel-larbi@users.noreply.github.com>
2022-11-23 08:44:55 +0000

    update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,15 @@
-  #include "main.h"
+#include "main.h"
 
-  /** 
-   * swap_int - swapping variables 
-   * @a: pointer 1
-   * @b: pointer 2
-   */
-   void swap_int(int *a, int *b) 
-  {
-     int swap;
+/**
+ * swap_int - swapping variables
+ * @a: pointer 1
+ * @b: pointer 2
+ */
+void swap_int(int *a, int *b)
+{
+  int swap;
 
-   swap = *a;
-   *a = *b;
-    *b = swap;
-
-  } 
+  swap = *a;
+  *a = *b;
+  *b = swap;
+}
